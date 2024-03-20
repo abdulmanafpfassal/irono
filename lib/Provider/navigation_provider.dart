a45340be75e58e9f0d72274c7f6de1322bfa8dd9
Abdul Manaf P Fassal <abdul.manaf@intbrains.com>
2024-03-20 11:08:42 +0530

    complete

--- lib/Provider/navigation_provider.dart
+++ lib/Provider/navigation_provider.dart
@@ -3,12 +3,19 @@ import 'package:flutter/material.dart';
 class NavigationProvider with ChangeNotifier {
 
   MENU initialPage = MENU.HOME;
+  BOOKING initialBooking = BOOKING.PENDING;
 
   setInitialPage(MENU page){
     initialPage = page;
     notifyListeners();
   }
 
+  setInitialBooking(BOOKING page){
+    initialBooking = page;
+    notifyListeners();
+  }
+
 }
 
 enum MENU { HOME, BOOKMARK, NOTIFICATION, MESSAGE }
+enum BOOKING {PENDING, ACCEPTED, COMPLETED}
